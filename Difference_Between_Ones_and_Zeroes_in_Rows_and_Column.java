a43c8cd2bd232d015795d6c6b8fff974286ab9f0
Satwik <116108258+theprogrammingfreak@users.noreply.github.com>
2023-03-25 00:03:08 +0530

    Create Difference_Between_Ones_and_Zeroes_in_Rows_and_Column.java

---
+++ Difference_Between_Ones_and_Zeroes_in_Rows_and_Column.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] onesMinusZeros(int[][] grid) {
+        int m=grid.length,n=grid[0].length,x;
+        int r[]=new int[m],c[]=new int[n];
+        for(int i=0;i<m;i++)
+        for(int j=0;j<grid[i].length;j++){
+            x=grid[i][j];
+            r[i]+=x;
+            c[j]+=x;
+        }
+        for(int i=0;i<m;i++)
+        for(int j=0;j<grid[i].length;j++)
+        grid[i][j]=2*(r[i]+c[j])-m-n;
+        return grid;
+    }
+}
