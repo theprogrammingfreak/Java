f728f3cd69f63ce11810dd957f304fb057879277
Satwik <116108258+theprogrammingfreak@users.noreply.github.com>
2023-04-23 22:42:24 +0530

    Create 1679. Max_Number_of_K-Sum_Pairs.java

---
+++ 1679. Max_Number_of_K-Sum_Pairs.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int ans = 0;
+        int i = 0;
+        int j = nums.length - 1;
+
+        while(i<j) {
+            if(nums[i] + nums[j] > k) {
+                j--;
+            } else if(nums[i] + nums[j] < k) {
+                i++;
+            } else {
+                ans++;
+                i++;
+                j--;
+            }
+        }
+        return ans;
+    }
+}
