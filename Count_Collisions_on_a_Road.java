0d4c69b8559d0b11fa00864c8cbe7fb4f2d29e8e
Satwik <116108258+theprogrammingfreak@users.noreply.github.com>
2023-06-10 00:02:19 +0530

    Update Count_Collisions_on_a_Road.java

--- Count_Collisions_on_a_Road.java
+++ Count_Collisions_on_a_Road.java
@@ -1,32 +1,23 @@
 class Solution {
-     public int countCollisions(String directions) {
-        
-        int collisions = 0;
-        Stack<Character> stack = new Stack();
-        stack.push(directions.charAt(0));
-        for(int i = 1;i<directions.length();i++){
-            char curr = directions.charAt(i);
-      
-            if((stack.peek()== 'R' && curr == 'L')  ){
-                
-                collisions+=2;
-                stack.pop();
-                curr = 'S';
-     
-
-            }else if((stack.peek() == 'S' && curr == 'L')){
-                curr = 'S';
-                collisions+=1;
-            }
-        
-            while(!stack.isEmpty() && ((stack.peek() == 'R' && curr == 'S') )){
-                collisions+=1;
-              stack.pop();
-            }
-            
-            stack.push(curr);
+    public int countCollisions(String directions) {
+        char[] arr = directions.toCharArray();
+        int left = 0;
+        int right = arr.length - 1;
+        while (left < arr.length && arr[left] == 'L') {
+            left++;
         }
-        
-        return collisions;
+        while (right >= 0 && arr[right] == 'R') {
+            right--;
+        }
+        if (left >= right) {
+            return 0;
+        }
+        int count = 0;
+        for (int i = left; i <= right; i++) {
+            if (arr[i] != 'S') {
+                count++;
+            }
+        }
+        return count;
     }
-} 
+}
