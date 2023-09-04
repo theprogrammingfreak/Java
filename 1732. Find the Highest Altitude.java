a54ceadc88eb2bd65d337300be913c64c1a2f6c2
Satwik <116108258+theprogrammingfreak@users.noreply.github.com>
2023-09-04 13:43:21 +0530

    Create 1732. Find the Highest Altitude.java

---
+++ 1732. Find the Highest Altitude.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int currAlt=0;
+        int maxAlt=0;
+        for(int i=0;i<gain.length;i++){
+            currAlt+=gain[i];
+            maxAlt=Math.max(maxAlt,currAlt);
+        }
+        return maxAlt;
+    }
+}
