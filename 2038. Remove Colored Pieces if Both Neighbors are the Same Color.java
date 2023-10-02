a47cb63943fced27bc1ccc5886526c22031e25ff
Satwik <116108258+theprogrammingfreak@users.noreply.github.com>
2023-10-02 21:56:33 +0530

    Update 2038. Remove Colored Pieces if Both Neighbors are the Same Color.java

--- 2038. Remove Colored Pieces if Both Neighbors are the Same Color.java
+++ 2038. Remove Colored Pieces if Both Neighbors are the Same Color.java
@@ -1,4 +1,4 @@
-class Solution {
+1.class Solution {
     public boolean winnerOfGame(String colors) {
         if(colors.length()<3) return false;
         String[] A=colors.split("B+");
@@ -14,6 +14,26 @@ class Solution {
             if(s.length()<3) continue;
             else{
                 Bcount+=s.length()-2;
+            }
+        }
+        return Acount > Bcount;
+    }
+}
+
+2.class Solution { //Efficient
+    public boolean winnerOfGame(String colors) {
+        int Acount=0,Bcount=0;
+        int A=0,B=0;
+        for(char c:colors.toCharArray()){
+            if (c == 'A') {
+                B=0;
+                A++;
+                if(A>=3) Acount++;
+            }
+            else{
+                A=0;
+                B++;
+                if(B>=3) Bcount++;
             }
         }
         return Acount > Bcount;
